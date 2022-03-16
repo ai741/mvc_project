372e0d4df52423d2f123de0ad92a7269f8588174
ai741 <link.4el@gmail.com>
2022-03-16 13:18:57 +0500

    Version 1.0.1

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_USERNAME = 'is341_16_admin';
+const DB_PASSWORD = 'eGtBf4uN5D';
+const DB_DATABASE = 'is341_16_1';
 const DB_PORT = '3306'; # Default 3306
