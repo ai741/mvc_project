88d6a86d03f0626e6958ca05f816ab3037bb4b41
ai741 <link.4el@gmail.com>
2022-03-19 10:34:54 +0500

    Version - 1.0.6 - work

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = '89.108.83.110';
 const DB_USERNAME = 'is341_16_admin';
-const DB_PASSWORD = 'eGtBf4uN5D';
+const DB_PASSWORD = 'wXkETD6bKA';
 const DB_DATABASE = 'is341_16_1';
 const DB_PORT = '3306'; # Default 3306
